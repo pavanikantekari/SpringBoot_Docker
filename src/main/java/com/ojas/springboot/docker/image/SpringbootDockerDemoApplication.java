fba50c7b40d7cf2ee149125107a23e6f415145d9
Pavani Kantekari <pk21074@corp.ojas-it.com>
2021-05-12 17:17:26 +0530

    SpringBoot_Docker

---
+++ src/main/java/com/ojas/springboot/docker/image/SpringbootDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ojas.springboot.docker.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerDemoApplication.class, args);
+	}
+
+}
